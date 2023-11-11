eb9b60353fa3ef6b8c6ee768b255ee81538e7dab
Janetta <osmonalievag@list.ru>
2023-11-11 19:26:03 +0600

    commit

--- pogrebok/wino/admin.py
+++ pogrebok/wino/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import grape,wine_sort,bochka
-admin.site.register(grape)
+from .models import grape,type_wine, wine_sort, bochka
+class grapeAdmin(admin.ModelAdmin):
+  list_display = ('title', 'date', 'place')
+  list_display_links = ('title',)
+  search_fields = ('title','date', 'place')
+  list_filter = ('date', 'place')
+
+
+admin.site.register(grape,grapeAdmin)
 admin.site.register(wine_sort)
 admin.site.register(bochka)
+admin.site.register(type_wine)
--- pogrebok/wino/models.py
+++ pogrebok/wino/models.py
@@ -3,35 +3,48 @@ from django.db import models
 
 # Create your models here.
 class grape(models.Model):
-    title=models.CharField(max_length=50,verbose_name='Название')
-    place=models.CharField(max_length=50,help_text='place',null=True, blank=True, )
-    date=models.DateField()   
+    title=models.CharField(max_length=50,verbose_name='Название сорта винограда')
+    place=models.CharField(max_length=50,null=True, blank=True,verbose_name='Место произрастания')
+    date=models.DateField(verbose_name='Время сбора',help_text='place')   
     def __str__(self):
         return self.title
     class Meta:
         verbose_name_plural = 'Сорта винограда'
         verbose_name = 'Сорт винограда'
         ordering = ['-title']
+
+class type_wine(models.Model):
+    title=models.CharField(max_length=50,verbose_name='Вид вина', blank=True, null=True)
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name_plural = 'Типы вина'
+        verbose_name = 'Тип вина'   
+     
+
 class wine_sort(models.Model):
-    title=models.CharField(max_length=50,verbose_name='hfghfg')
-    type=models.CharField(max_length=50)
-    color=models.CharField(max_length=50)
-    b_year=models.DurationField()
-    bu_month=models.DurationField()
-    s_grape=models.ForeignKey(grape,on_delete = models.CASCADE)
+    title=models.CharField(max_length=50,verbose_name='Название вина')
+    
+    # type=models.CharField(max_length=50,verbose_name='Тип')
+    color=models.CharField(max_length=50,verbose_name='Цвет')
+    b_year=models.DurationField(verbose_name='Время выдержки в бочках', help_text='в годах')
+    bu_month=models.DurationField(verbose_name='Время выдержки в бутылках', help_text='в месяцах')
+    s_grape=models.ForeignKey(grape,on_delete = models.CASCADE,verbose_name='Сорт винограда')
+    type1=models.ForeignKey(type_wine,on_delete = models.CASCADE,verbose_name='тип винограда',blank=True, null=True)
     def __str__(self):
         return self.title
     class Meta:
         verbose_name_plural = 'Сорта вин'
         verbose_name = 'Сорт вина'
         ordering = ['-title']
+
 class bochka(models.Model):
-    mesto=models.CharField(max_length=50,verbose_name='hfghfg')
-    date_izg=models.DateField()   
-    obyem=models.FloatField()
-    pusto=models.BooleanField()
-    data_zapolnenya=models.DateField()
-    sort_vina=models.ForeignKey(wine_sort,on_delete = models.CASCADE)
+    mesto=models.CharField(max_length=50,verbose_name='')
+    date_izg=models.DateField(verbose_name='Дата изготовления')   
+    obyem=models.FloatField(verbose_name='Объем')
+    pusto=models.BooleanField(verbose_name='Пустота')
+    data_zapolnenya=models.DateField(verbose_name='Дата заполнения бочки')
+    sort_vina=models.ForeignKey(wine_sort,on_delete = models.CASCADE,verbose_name='Сорт вина')
     def __str__(self):
         return self.title
     class Meta:
